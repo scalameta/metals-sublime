a42e8f6f0aeeb76d2fee915279a5248d1a61109f
Ayoub Benali <a.benali@dcmn.com>
2019-11-15 21:11:51 +0100

    pass the server settings

--- plugin.py
+++ plugin.py
@@ -6,6 +6,8 @@ from LSP.plugin.core.settings import ClientConfig, LanguageConfig
 
 server_name = 'metals-sublime'
 settings_file = 'metals-sublime.sublime-settings'
+coursierPath = os.path.join(os.path.dirname(__file__), './coursier')
+
 def get_java_path(java_path: 'Optional[str]') -> 'Optional[str]':
     if java_path:
         return java_path + "/bin/java"
@@ -14,23 +16,8 @@ def get_java_path(java_path: 'Optional[str]') -> 'Optional[str]':
     else:
         return None
 
-def plugin_loaded():
-    plugin_settings = sublime.load_settings(settings_file)
-    java_path = get_java_path(plugin_settings.get('java_home'))
-    if not java_path :
-        window.status_message("Please install java or set the 'java_home' setting")
-
-    server_version = plugin_settings.get('server_version')
-    if not server_version :
-        window.status_message("'server_version' should be set")
-
-def create_launch_command(artifactVersion: str):
-    coursierPath = os.path.join(os.path.dirname(__file__), './coursier')
-    command = [
-        "java",
-        "-Dmetals.client=sublime",
-        "-Xss4m",
-        "-Xms100m",
+def create_launch_command(java_path: str, artifactVersion: str, serverProperties: 'List[str]') -> 'List[str]':
+    command = [java_path] + serverProperties + [
         "-jar",
         coursierPath,
         "launch",
@@ -51,7 +38,12 @@ class LspMetalsPlugin(LanguageHandler):
         plugin_settings = sublime.load_settings(settings_file)
         server_version = plugin_settings.get('server_version')
         server_properties = prepare_server_properties(plugin_settings.get('server_properties', []))
-        launch_command = create_launch_command(server_version)
+        java_path = get_java_path(plugin_settings.get('java_home'))
+
+        launch_command = []
+        if java_path and server_version :
+            launch_command = create_launch_command(java_path, server_version, server_properties)
+
         metals_config = ClientConfig(
             name=server_name,
             binary_args=launch_command,
@@ -79,6 +71,17 @@ class LspMetalsPlugin(LanguageHandler):
         return self._config
 
     def on_start(self, window) -> bool:
+        plugin_settings = sublime.load_settings(settings_file)
+        java_path = get_java_path(plugin_settings.get('java_home'))
+        if not java_path :
+            window.status_message("Please install java or set the 'java_home' setting")
+            return False
+
+        server_version = plugin_settings.get('server_version')
+        if not server_version :
+            window.status_message("'server_version' setting should be set")
+            return False
+
         return True
 
     def on_initialized(self, client) -> None:
@@ -90,7 +93,6 @@ def register_client(client):
         "metals/status",
         lambda params: on_metals_status(params))
 
-
 def on_metals_status(params):
     view = sublime.active_window().active_view()
     hide = params.get('hide', False)
@@ -99,11 +101,7 @@ def on_metals_status(params):
     else:
         view.set_status(server_name, params.get('text', ''))
 
-def prepare_server_properties(properties: 'List[str]') -> str:
+def prepare_server_properties(properties: 'List[str]') -> 'List[str]':
     stripped = map(lambda p: p.strip(), properties)
     none_empty = list(filter(None, stripped))
-    result = ''
-    if none_empty:
-        result = ' '.join(none_empty)
-
-    return result
+    return none_empty
