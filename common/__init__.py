61dc14d5bf883bfe1d03ed501f46300dad13a357
Ayoub Benali <benaliayoub@outlook.com>
2021-02-13 17:04:54 +0100

    Add clickable button to show the hoverContent in a popup

--- common/__init__.py
+++ common/__init__.py
@@ -1,12 +1,13 @@
-from LSP.plugin.core.types import Optional, List
-from LSP.plugin.core.url import filename_to_uri  # TODO: deprecated in a future version
-from LSP.plugin.execute_command import LspExecuteCommand  # TODO: bring to public API
-from LSP.plugin.core.views import range_to_region
-from LSP.plugin.core.types import Optional, Dict, Any, Tuple, List
+from LSP.plugin.core.css import css
 from LSP.plugin.core.protocol import Range
+from LSP.plugin.core.types import Optional, Dict, Any, Tuple, List
+from LSP.plugin.core.url import filename_to_uri  # TODO: deprecated in a future version
+from LSP.plugin.core.views import range_to_region, FORMAT_MARKED_STRING, FORMAT_MARKUP_CONTENT, minihtml
+from LSP.plugin.execute_command import LspExecuteCommand  # TODO: bring to public API
 import os
 import sublime
 import sublime_plugin
+import mdpopups
 from functools import reduce
 
 _COURSIER_PATH = os.path.join(os.path.dirname(__file__), '..', 'coursier')
@@ -49,16 +50,41 @@ def deep_get(dictionary: Dict[str, Any], *keys):
 
 PHANTOM_HTML = """
 <style>div.phantom {{font-style: italic; color: {}}}</style>
-<div class='phantom'>{}</div>"""
+<div class='phantom'>{} {}</div>"""
+
+def show_popup(content: Dict[str, Any], view: sublime.View, location: int):
+    html = minihtml(view, content, allowed_formats=FORMAT_MARKED_STRING | FORMAT_MARKUP_CONTENT)
+    mdpopups.show_popup(
+        view,
+        html,
+        css=css().popups,
+        md=False,
+        flags=sublime.HIDE_ON_MOUSE_MOVE_AWAY,
+        location=location,
+        wrapper_class=css().popups_classname,
+        max_width=800,
+        on_navigate=None)
+
 
 def decoration_to_phantom(option: Dict[str, Any], view: sublime.View) -> Optional[sublime.Phantom]:
-    region = range_to_region(Range.from_lsp(option['range']), view)
+    decorationRange = Range.from_lsp(option['range'])
+    region = range_to_region(decorationRange, view)
     region.a = region.b  # make the start point equal to the end point
-
     hoverMessage = deep_get(option, 'hoverMessage')
     contentText = deep_get(option, 'renderOptions', 'after', 'contentText')
+    link = ''
+    point = None
+    if hoverMessage:
+        link = "<a href='more'>more</a>"
+        point = view.text_point(decorationRange.start.row, decorationRange.start.col)
+
     color = view.style_for_scope("comment")["foreground"]
-    phantom = sublime.Phantom(region, PHANTOM_HTML.format(color, contentText), sublime.LAYOUT_INLINE, None)
+    phantomContent = PHANTOM_HTML.format(color, contentText, link)
+    phantom = sublime.Phantom(
+        region,
+        phantomContent,
+        sublime.LAYOUT_INLINE,
+        lambda href: show_popup(hoverMessage, view, point))
 
     return phantom
 
