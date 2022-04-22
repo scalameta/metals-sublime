50af1d084d94a2ee987978d43e483b42bb45439a
Ayoub Benali <ayoub-benali@users.noreply.github.com>
2022-04-22 11:44:20 +0100

    Implement metals-goto-location command to fix stacktrace analysis links (#88)
    * Implement metals-goto-location command
    
    Given that Metals uses it for stacktrace analysis since https://github.com/scalameta/metals/pull/3540
    
    * Remove replaced command
    
    Co-authored-by: Ayoub Benali <benaliayoub@outlook.com>

--- commands/lsp_metals_metals_goto_location.py
+++ commands/lsp_metals_metals_goto_location.py
@@ -3,7 +3,7 @@ from LSP.plugin.core.types import List, Any
 
 import sublime_plugin
 
-class LspMetalsGotoPositionCommand(sublime_plugin.WindowCommand):
+class LspMetalsMetalsGotoLocationCommand(sublime_plugin.WindowCommand):
 
     def run(self, parameters: List[Any]) -> None:
         if isinstance(parameters, list) and parameters:
