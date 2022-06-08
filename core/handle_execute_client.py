1130a84e3e62a1ae4c35da4b288f542ee5489ac4
Justin Lam <4069123+ninth-dev@users.noreply.github.com>
2022-06-08 16:55:10 +1000

    Handle doctor new format and versions (#93)
    * Handle new metal doctor format
    
    - Add build server information
    - Add JDK information
    - Add metals server information
    - Add explanations
    - Remove tabulate
    
    * Update to support all doctor versions
    
    * Update target labels / fields to display in order

--- core/handle_execute_client.py
+++ core/handle_execute_client.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from . status import status_key
 from .. commands.utils import open_location
 from LSP.plugin.core.sessions import Session
@@ -5,7 +6,6 @@ from LSP.plugin.core.types import Any
 import json
 import sublime
 import mdpopups
-from tabulate import tabulate
 
 
 def handle_execute_client(session: Session, params: Any) -> None:
@@ -40,11 +40,34 @@ def show_stacktrace(session: Session, args: Any) -> None:
 
 def run_doctor(session: Session, args: Any) -> None:
     if isinstance(args, list) and args:
+
         content = json.loads(args[0])
+
+        doctor_version = 0 if content.get('version') is None else int(content.get('version'))
+
+        header = content.get('header')
         messages = content.get('messages')
         targets = content.get('targets')
-        markdown = ""
-        markdown += "## {}  \n\n".format(content.get('headerText'))
+        explanations = content.get('explanations')
+
+        markdown = "# {} \n\n".format(content.get('title'))
+
+        if doctor_version >= 3:
+            fields = [
+              'buildTool',
+              'buildServer',
+              'importBuildStatus',
+              'jdkInfo',
+              'serverInfo'
+            ]
+
+            for field in fields:
+              if header.get(field):
+                markdown += "{} \n\n".format(header.get(field))
+
+            markdown += "{} \n\n".format(header.get('buildTargetDescription'))
+        else:
+            markdown += "{} \n\n".format(content.get('headerText'))
 
         if messages:
             for message in messages:
@@ -52,28 +75,55 @@ def run_doctor(session: Session, args: Any) -> None:
                 for recommendation in message.get('recommendations'):
                     markdown += '* {}\n'.format(recommendation)
                 markdown += '\n\n'
-        else:
-            headers = [
-                'Build Target',
-                'Scala Version',
-                'Diagnostics',
-                'Goto Definition',
-                'Completions',
-                'Find References',
-                'Recommendation'
-            ]
+        elif targets:
             markdown += "## Build Targets\n"
+
+            default_target_labels = OrderedDict([
+                ('targetType', 'Target Type'),
+                ('compilationStatus', 'Compilation status'),
+                ('diagnostics', 'Diagnostics'),
+                ('interactive', 'Interactive'),
+                ('semanticdb', 'SemanticDB'),
+                ('debugging', 'Debugging'),
+                ('java', 'Java Support'),
+                ('recommendation', 'Recommendation')
+            ])
+
+            legacy_target_labels = OrderedDict([
+                ('scalaVersion', 'Scala Version'),
+                ('diagnostics', 'Diagnostics'),
+                ('gotoDefinition', 'Goto Definition'),
+                ('completions', 'Completions'),
+                ('findReferences', 'Find References')
+            ])
+
+            target_labels = default_target_labels if doctor_version > 0 else legacy_target_labels
+
             lines = []
             for target in targets:
-                lines.append([
-                    target.get('buildTarget'),
-                    target.get('scalaVersion'),
-                    target.get('diagnostics'),
-                    target.get('gotoDefinition'),
-                    target.get('completions'),
-                    target.get('findReferences'),
-                    target.get('recommendation')
-                ])
-            table = tabulate(lines, headers, "pretty")
-            markdown += "```\n{}\n```\n\n".format(table)
-        mdpopups.new_html_sheet(session.window, "Metals Doctor", markdown, True)
+                lines.append('#### {}'.format(target.get('buildTarget')))
+                lines.append('```')
+
+                for field, label in target_labels.items():
+                    if target.get(field):
+                        lines.append('* {0:<20}: {1}'.format(label, target.get(field)))
+
+                lines.append("```")
+
+            markdown += "{}\n\n".format('\n'.join(lines))
+
+            if explanations:
+                markdown += "## Explanations\n"
+                for explanation in explanations:
+                    markdown += '{}\n\n'.format(explanation.get('title'))
+                    for deep_explanation in explanation.get('explanations'):
+                        markdown += '* {}\n'.format(deep_explanation)
+                    markdown += '\n\n'
+
+        custom_css = """
+        .metals-doctor { padding: 1.5rem }
+        .metals-doctor h1, .metals-doctor h2 { text-decoration: underline }
+        .metals-doctor h2, .metals-doctor h3, .metals-doctor h4, .metals-doctor p, .metals-doctor ul { margin-top: 1rem }
+        """
+
+        mdpopups.new_html_sheet(session.window, "Metals Doctor", markdown, True, css=custom_css, wrapper_class="metals-doctor")
