4f8f4e541f41b6ba1e1a08d800a63aaf1cf298fa
Ayoub Benali <benaliayoub@outlook.com>
2022-09-17 18:46:12 +0200

    Add run scalafix command

--- plugin.py
+++ plugin.py
@@ -16,17 +16,18 @@ elif sublime.version() < '4000':
     sublime.error_message('This version requires st4, use the st3 branch')
 else:
     from . commands.lsp_metals_analyze_stacktrace import LspMetalsAnalyzeStacktraceCommand
+    from . commands.lsp_metals_copy_worksheet import LspMetalsCopyWorksheetCommand
     from . commands.lsp_metals_execute_command import LspMetalsExecuteCommand
     from . commands.lsp_metals_file_decoder import LspMetalsFileDecoderCommand
     from . commands.lsp_metals_find_in_dependency import LspMetalsFindInDependencyCommand
     from . commands.lsp_metals_focus import LspMetalsFocusViewCommand, ActiveViewListener
     from . commands.lsp_metals_goto import LspMetalsGoto
-    from . commands.lsp_metals_metals_goto_location import LspMetalsMetalsGotoLocationCommand
     from . commands.lsp_metals_goto_super_method import LspMetalsSendPositionCommand
+    from . commands.lsp_metals_metals_goto_location import LspMetalsMetalsGotoLocationCommand
+    from . commands.lsp_metals_run_scalafix import LspMetalsRunScalafixCommand
     from . commands.lsp_metals_show_build_target_info import LspMetalsShowBuildTargetInfoCommand
     from . commands.lsp_metals_text_command import LspMetalsTextCommand
     from . core.decorations import WorksheetListener, LspMetalsClearPhantomsCommand
-    from . commands.lsp_metals_copy_worksheet import LspMetalsCopyWorksheetCommand
     from . core.metals import Metals
 
     def plugin_loaded() -> None:
