6a60ff7e05da9d5f83adf08427f515605b592074
Предраг Николић <idmpepe@gmail.com>
2026-03-28 17:15:11 +0100

    Optional is no longer exported from LSP, import it directly from typing

--- core/handle_input_box.py
+++ core/handle_input_box.py
@@ -1,5 +1,5 @@
 from LSP.plugin import Response, Session
-from LSP.plugin.core.types import Optional, Any
+from typing import Optional, Any
 
 
 def handle_input_box(session: Session, params: Any, request_id: Any) -> None:
